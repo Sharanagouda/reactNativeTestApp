1a86ee97214104c2d3e8808f195854d4aa9df574
sharanagouda <sharanu.mdl@gmail.com>
2020-03-24 15:16:15 +0530

    added a tinder components

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-import {Router, Stack,Scene,Tabs} from "react-native-router-flux";
+import { Router, Stack, Scene, Tabs } from "react-native-router-flux";
 import Home from "../screens/Home";
 import Login from "../screens/Login";
-
+import TinderApp from "../screens/tinderapp"
 
 export default class Routes extends Component {
     render() {
         return (
             <Router>
                 <Stack key="root">
-                <Scene component={Login} initial={true} title="Login"/>
-                    <Scene component={Home} title="HOME"/>
+                    <Scene component={TinderApp} initial={true} title="tinder" />
+                    <Scene component={Home} title="HOME" />
+                    <Scene component={Login} title="Login" />
                 </Stack>
             </Router>
         );
---
+++ src/constants/userData.js
@@ -0,0 +1,87 @@
+const photoCards = [
+    {
+        name: 'Austin Wade',
+        age: 22,
+        photo: require('../Images/assets/0.jpeg'),
+        key: 'caseex6qfO4TPMYyhorner',
+    },
+    {
+        name: 'Aleksander Borzenets',
+        age: 28,
+        photo: require('../Images/assets/0_acfZhsMVju-t_8na.jpg'),
+        key: 'ozda-XbeP0k',
+    },
+    {
+        name: 'Don Delfin Espino',
+        age: 29,
+        photo: require('../Images/assets/download (1).jpeg'),
+        key: 'nBywXevf_jE-',
+    },
+    {
+        name: 'Eduardo Dutra',
+        age: 30,
+        photo: require('../Images/assets/download (2).jpeg'),
+        key: 'ZHy0efLnzVc',
+    },
+    {
+        name: 'Wesley Tingey',
+        age: 21,
+        photo: require('../Images/assets/download (3).jpeg'),
+        key: 'TvPCUHten1o',
+    },
+    {
+        name: 'Gift Habeshaw',
+        age: 26,
+        photo: require('../Images/assets/download.jpeg'),
+        key: 'dlbiYGwEe9U',
+    },
+    {
+        name: 'Henri Pham',
+        age: 30,
+        photo: require('../Images/assets/henri-pham-Ml4tr2WO7JE-unsplash.jpg'),
+        key: 'Ml4tr2WO7JE',
+    },
+    {
+        name: 'Nico Marks',
+        age: 24,
+        photo: require('../Images/assets/nico-marks-mFcc5b_t74Q-unsplash.jpg'),
+        key: 'mFcc5b_t74Q',
+    },
+    {
+        name: 'Sirio',
+        age: 28,
+        photo: require('../Images/assets/r1.jpeg'),
+        key: "Ty4f_NOFO60'",
+    },
+    {
+        name: 'Teymi Townsend',
+        age: 30,
+        photo: require('../Images/assets/r2.jpeg'),
+        key: "AvLHH8qYbAI'",
+    },
+    {
+        name: 'Caique Silva',
+        age: 20,
+        photo: require('../Images/assets/r3.jpeg'),
+        key: "3ujVzg9i2EI'",
+    },
+    {
+        name: 'David Yanutenama',
+        age: 21,
+        photo: require('../Images/assets/r3.jpeg'),
+        key: "5AoO7dBurMw'",
+    },
+    {
+        name: 'David Yanutenama',
+        age: 21,
+        photo: require('../Images/assets/r4.jpeg'),
+        key: "5AoO7dBurMw'",
+    },
+    {
+        name: 'David Yanutenama',
+        age: 21,
+        photo: require('../Images/assets/r4.jpeg'),
+        key: "5AoO7dBurMw'",
+    },
+]
+export default photoCards
